7f739e7c058ab11ec131e42645a347273c8c231a
Maick Souza <maick_a_s@msn.com>
2022-01-07 16:54:27 -0300

    feat(server): challenge done!

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -10,21 +10,29 @@ export class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
-
+ 
   async findUserWithGamesById({
     user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  }: IFindUserWithGamesDTO): Promise<User> { 
+    const user = await this.repository.findOneOrFail(
+      { id: user_id },
+      { relations: ["games"]}
+    )
+    return user
+
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query("SELECT * FROM USERS ORDER BY first_name ASC"); 
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      "SELECT * FROM users WHERE LOWER(first_name) = LOWER($1) AND LOWER(last_name) = lOWER($2)",
+      [first_name, last_name]
+      );
   }
 }
